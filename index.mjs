ba59ab1a9fa0718d868e6e79177259190830a55a
Anyer Castillo <anyer.castillo@ditto.live>
2023-10-15 16:12:30 -0400

    Add btn

--- index.mjs
+++ index.mjs
@@ -5,22 +5,32 @@ import OnOff from "onoff";
 const properties = PropertiesReader('local.properties');
 const DITTO_APP_ID= properties.get("DITTO_APP_ID");
 const DITTO_APP_TOKEN= properties.get("DITTO_APP_TOKEN");
-const LED = new OnOff.Gpio(4, 'out');
+const LED_A = new OnOff.Gpio(4, 'out');
+const BTN_A = new OnOff.Gpio(14, 'in');
 
-let ditto;
+var btnA = 0;
 
-async function main() {
-  await init()
+const readWriteIO = () => {
+  const valueBtnA = BTN_A.readSync();
 
-  ditto = new Ditto({
+  if (valueBtnA !== btnA) {
+    btnA = valueBtnA;
+  }
+
+  LED_A.writeSync(btnA);
+}
+
+const main = async () => {
+  await init();
+
+  const ditto = new Ditto({
     type: "onlinePlayground",
     appID: DITTO_APP_ID,
     token: DITTO_APP_TOKEN,
   });
 
   ditto.startSync();
-
-  LED.writeSync(1);
+  setInterval(readWriteIO, 100);
 }
 
-main()
+main();
