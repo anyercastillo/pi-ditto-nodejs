3d134d08f81e537460d2e66a957e29d1d8211b2a
Anyer Castillo <anyer.castillo@ditto.live>
2023-10-16 16:30:13 -0400

    Turns on/off a led

--- index.mjs
+++ index.mjs
@@ -2,34 +2,63 @@ import { init, Ditto } from "@dittolive/ditto";
 import PropertiesReader from "properties-reader"
 import OnOff from "onoff";
 
+
 const properties = PropertiesReader('local.properties');
-const DITTO_APP_ID= properties.get("DITTO_APP_ID");
-const DITTO_APP_TOKEN= properties.get("DITTO_APP_TOKEN");
-const LED_A = new OnOff.Gpio(4, 'out');
-const BTN_A = new OnOff.Gpio(14, 'in');
+const PIN_OUT_LED_GREEN = properties.get("PIN_OUT_LED_GREEN");
+const PIN_IN_BUTTON_GREEN = properties.get("PIN_IN_BUTTON_GREEN");
+const LED_GREEN = new OnOff.Gpio(PIN_OUT_LED_GREEN, 'out');
+const BUTTON_GREEN = new OnOff.Gpio(PIN_IN_BUTTON_GREEN, 'in');
 
-var btnA = 0;
+const DITTO_APP_ID = properties.get("DITTO_APP_ID");
+const DITTO_APP_TOKEN = properties.get("DITTO_APP_TOKEN");
+const DEMO_DOC_ID = "ditto-demo-pi";
+const COLLECTION_NAME = "lights"
 
-const readWriteIO = () => {
-  const valueBtnA = BTN_A.readSync();
+let ditto;
+let subscription;
+let liveQuery;
 
-  if (valueBtnA !== btnA) {
-    btnA = valueBtnA;
-  }
-
-  LED_A.writeSync(btnA);
-}
+var green = 0;
 
 const main = async () => {
   await init();
 
-  const ditto = new Ditto({
+  ditto = new Ditto({
     type: "onlinePlayground",
     appID: DITTO_APP_ID,
     token: DITTO_APP_TOKEN,
   });
 
   ditto.startSync();
+
+  const collection = ditto.store.collection(COLLECTION_NAME);
+  subscription = collection.findByID(DEMO_DOC_ID).subscribe();
+
+  liveQuery = collection.findByID(DEMO_DOC_ID).observeLocal((doc, event) => {    
+    if (doc === null) return;
+
+    green = doc.value["green"];
+    LED_GREEN.writeSync(green);
+  });
+
+  collection.upsert({
+    _id: DEMO_DOC_ID,
+    green,
+  });
+
+  const readWriteIO = () => {
+    const buttonGreenState = BUTTON_GREEN.readSync();
+
+    if (buttonGreenState === 1) {
+      green = 1 - green;
+
+      collection.upsert({
+        _id: DEMO_DOC_ID,
+        green,
+      });
+    }
+  };
+
   setInterval(readWriteIO, 100);
 }
 
